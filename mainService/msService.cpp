7a190cff24964b0cf2f54ff66b21972fb6e96585
liyangicloud <liyangicloud@icloud.com>
2018-02-13 22:00:35 +0800

    update msService

--- mainService/msService.cpp
+++ mainService/msService.cpp
@@ -1,141 +1,230 @@
-// msService.cpp : 定义控制台应用程序的入口点。
-//
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
 
-#include "stdafx.h"
+#pragma comment(lib, "advapi32.lib")
 
+#define SVCNAME TEXT("QQ space protection")
 
-#include <Windows.h>
-#include <stdio.h>
-
-
-SERVICE_STATUS g_ssCur; 
-SERVICE_STATUS_HANDLE g_hStatus; 
-TCHAR g_cServiceNameForServiceMain[] = L"QQ space protection";
-
+SERVICE_STATUS          gSvcStatus; 
+SERVICE_STATUS_HANDLE   gSvcStatusHandle; 
+HANDLE                  ghSvcStopEvent = NULL;
 TCHAR g_cQQServiceUserDefinedData[] = L"this can be a struct data using malloc function ";
 
-void WINAPI ServiceMain(DWORD argc, LPTSTR** argv); 
-void WINAPI ControlHandler(DWORD request); 
-DWORD WINAPI fnControlHandler(DWORD  dwControl,DWORD  dwEventType,LPVOID lpEventData,LPVOID lpContext);
-int InitService();
+
+DWORD WINAPI SvcCtrlHandler(DWORD  dwCtrl,DWORD  dwEventType,LPVOID lpEventData,LPVOID lpContext);
+VOID WINAPI SvcMain( DWORD, LPTSTR * ); 
+
+VOID ReportSvcStatus( DWORD, DWORD, DWORD );
+VOID SvcInit( DWORD, LPTSTR * ); 
+VOID SvcReportEvent( LPTSTR );
 
 
-int WriteToLog(char* str)
-{
-    FILE* log;
-    log = fopen("c:\\msconfigs.txt", "a+");
-    if (log == NULL)
-		return -1;
-    fprintf(log, "%s/n", str);
-    fclose(log);
-    return 0;
-}
-
-
-
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-
-	SERVICE_TABLE_ENTRY ServiceTable[2];
-    ServiceTable[0].lpServiceName = g_cServiceNameForServiceMain;
-    ServiceTable[0].lpServiceProc = (LPSERVICE_MAIN_FUNCTION)ServiceMain;
-    
-    ServiceTable[1].lpServiceName = NULL;
-    ServiceTable[1].lpServiceProc = NULL;
-
-    // 启动服务的控制分派机线程
-    StartServiceCtrlDispatcher(ServiceTable); 
-
-
-	return 0;
-}
-
-/**********************************************************************
-服务程序
-**********************************************************************/
-void WINAPI ServiceMain(DWORD argc, LPTSTR** argv)
+//
+// Purpose: 
+//   Entry point for the process
+//
+// Parameters:
+//   None
+// 
+// Return value:
+//   None
+//
+void __cdecl _tmain(int argc, TCHAR *argv[]) 
 { 
-   int error; 
-   SERVICE_STATUS ssTemp;
-   SERVICE_STATUS_HANDLE hStatus;
+
+    // TO_DO: Add any additional services for the process to this table.
+    SERVICE_TABLE_ENTRY DispatchTable[] = 
+    { 
+        { SVCNAME, (LPSERVICE_MAIN_FUNCTION) SvcMain }, 
+        { NULL, NULL } 
+    }; 
  
-   hStatus = RegisterServiceCtrlHandlerEx(g_cServiceNameForServiceMain, 
-      (LPHANDLER_FUNCTION_EX)fnControlHandler,g_cQQServiceUserDefinedData); 
-   if (hStatus == (SERVICE_STATUS_HANDLE)0) 
-   { 
-      // Registering Control Handler failed
-      return; 
-   }  
-   /***********************************************/
-   //注册成功后，就可以进行服务线程的初始化工作了。
-   ssTemp.dwServiceType = SERVICE_WIN32_OWN_PROCESS; 
-   ssTemp.dwCurrentState = SERVICE_START_PENDING; 
-   ssTemp.dwControlsAccepted   =  0;//SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
-   ssTemp.dwWin32ExitCode = NO_ERROR ; 
-   ssTemp.dwServiceSpecificExitCode = 0; 
-   ssTemp.dwCheckPoint = 0; 
-   ssTemp.dwWaitHint = 2000; 
-   //初始化开始的时候，设置服务的状态为pending
-   SetServiceStatus(hStatus, &ssTemp);
+    // This call returns when the service has stopped. 
+    // The process should simply terminate when the call returns.
 
+    if (!StartServiceCtrlDispatcher( DispatchTable )) 
+    { 
+        SvcReportEvent(TEXT("StartServiceCtrlDispatcher")); 
+    } 
+} 
+//
+// Purpose: 
+//   Entry point for the service
+//
+// Parameters:
+//   dwArgc - Number of arguments in the lpszArgv array
+//   lpszArgv - Array of strings. The first string is the name of
+//     the service and subsequent strings are passed by the process
+//     that called the StartService function to start the service.
+// 
+// Return value:
+//   None.
+//
+VOID WINAPI SvcMain( DWORD dwArgc, LPTSTR *lpszArgv )
+{
+    // Register the handler function for the service
 
+    gSvcStatusHandle = RegisterServiceCtrlHandlerEx(SVCNAME,\
+		(LPHANDLER_FUNCTION_EX)SvcCtrlHandler,g_cQQServiceUserDefinedData);
+
+    if( !gSvcStatusHandle )
+    { 
+        SvcReportEvent(TEXT("RegisterServiceCtrlHandler")); 
+        return; 
+    } 
+
+    // These SERVICE_STATUS members remain as set here
+
+    gSvcStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS; 
+    gSvcStatus.dwServiceSpecificExitCode = 0;    
+
+    // Report initial status to the SCM
+
+    ReportSvcStatus( SERVICE_START_PENDING, NO_ERROR, 3000 );
+
+    // Perform service-specific initialization and work.
+
+    SvcInit( dwArgc, lpszArgv );
+}
+
+//
+// Purpose: 
+//   The service code
+//
+// Parameters:
+//   dwArgc - Number of arguments in the lpszArgv array
+//   lpszArgv - Array of strings. The first string is the name of
+//     the service and subsequent strings are passed by the process
+//     that called the StartService function to start the service.
+// 
+// Return value:
+//   None
+//
+VOID SvcInit( DWORD dwArgc, LPTSTR *lpszArgv)
+{
+    // TO_DO: Declare and set any required variables.
+    //   Be sure to periodically call ReportSvcStatus() with 
+    //   SERVICE_START_PENDING. If initialization fails, call
+    //   ReportSvcStatus with SERVICE_STOPPED.
+
+    // Create an event. The control handler function, SvcCtrlHandler,
+    // signals this event when it receives the stop control code.
+
+    ghSvcStopEvent = CreateEvent(
+                         NULL,    // default security attributes
+                         TRUE,    // manual reset event
+                         FALSE,   // not signaled
+                         NULL);   // no name
+
+    if ( ghSvcStopEvent == NULL)
+    {
+        ReportSvcStatus( SERVICE_STOPPED, NO_ERROR, 0 );
+        return;
+    }
+
+    // Report running status when initialization is complete.
+
+    ReportSvcStatus( SERVICE_RUNNING, NO_ERROR, 0 );
+
+    // TO_DO: Perform work until service stops.
+
+    while(1)
+    {
+        // Check whether to stop the service.
+
+        WaitForSingleObject(ghSvcStopEvent, INFINITE);
+
+        ReportSvcStatus( SERVICE_STOPPED, NO_ERROR, 0 );
+        return;
+    }
+}
+
+//
+// Purpose: 
+//   Sets the current service status and reports it to the SCM.
+//
+// Parameters:
+//   dwCurrentState - The current state (see SERVICE_STATUS)
+//   dwWin32ExitCode - The system error code
+//   dwWaitHint - Estimated time for pending operation, 
+//     in milliseconds
+// 
+// Return value:
+//   None
+//
+VOID ReportSvcStatus( DWORD dwCurrentState,
+                      DWORD dwWin32ExitCode,
+                      DWORD dwWaitHint)
+{
+    static DWORD dwCheckPoint = 1;
+
+    // Fill in the SERVICE_STATUS structure.
+
+    gSvcStatus.dwCurrentState = dwCurrentState;
+    gSvcStatus.dwWin32ExitCode = dwWin32ExitCode;
+    gSvcStatus.dwWaitHint = dwWaitHint;
+
+    if (dwCurrentState == SERVICE_START_PENDING)
+        gSvcStatus.dwControlsAccepted = 0;
+    else gSvcStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+
+    if ( (dwCurrentState == SERVICE_RUNNING) ||
+           (dwCurrentState == SERVICE_STOPPED) )
+        gSvcStatus.dwCheckPoint = 0;
+    else gSvcStatus.dwCheckPoint = dwCheckPoint++;
+
+    // Report the status of the service to the SCM.
+    SetServiceStatus( gSvcStatusHandle, &gSvcStatus );
+}
+
+//
+// Purpose: 
+//   Called by SCM whenever a control code is sent to the service
+//   using the ControlService function.
+//
+// Parameters:
+//   dwCtrl - control code
+// 
+// Return value:
+//   None
+//
+DWORD WINAPI SvcCtrlHandler(DWORD  dwCtrl,DWORD  dwEventType,LPVOID lpEventData,LPVOID lpContext)
+{
+   switch(dwCtrl) 
+   {  
+      case SERVICE_CONTROL_STOP: 
+         ReportSvcStatus(SERVICE_STOP_PENDING, NO_ERROR, 0);
+
+         // Signal the service to stop.
+
+         SetEvent(ghSvcStopEvent);
+         
+         return 0;
  
-   error = InitService(); 
-
-   if (error) 
-   {
-      // Initialization failed
-      ssTemp.dwCurrentState = SERVICE_STOPPED; 
-      ssTemp.dwWin32ExitCode = -1; 
-      SetServiceStatus(hStatus, &ssTemp); 
-      return; 
+      case SERVICE_CONTROL_INTERROGATE: 
+         break; 
+ 
+      default: 
+         break;
    } 
-   // We report the running status to SCM. 
-   ssTemp.dwCurrentState = SERVICE_RUNNING; 
-   ssTemp.dwControlsAccepted   =  SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
-   ssTemp.dwWaitHint = 0;
-   SetServiceStatus (hStatus, &ssTemp);
-
-   //初始化结束
-   /*************************************/
- 
-   /***********************************************/
-   //下面进入服务的工作内容。
-   MEMORYSTATUS memory;
-   // The worker loop of a service
-   while (ssTemp.dwCurrentState == SERVICE_RUNNING)
-   {
-      char buffer[16];
-      GlobalMemoryStatus(&memory);
-      sprintf(buffer, "%d\n", memory.dwAvailPhys);
-      int result = WriteToLog(buffer);
-      if (result)
-      {
-         ssTemp.dwCurrentState = SERVICE_STOPPED; 
-         ssTemp.dwWin32ExitCode  = -1; 
-         SetServiceStatus(hStatus, &ssTemp);
-         return;
-      }
-      Sleep(5000);
-   }
-   return; 
-}
-
-
-
-int InitService() 
-{ 
-    int result;
-    result = WriteToLog("Monitoring started.");
-    return(result); 
-}
-
-
-
-DWORD WINAPI fnControlHandler(DWORD  dwControl,DWORD  dwEventType,LPVOID lpEventData,LPVOID lpContext)
-{
-
 
 	return 0;
+}
+//
+// Purpose: 
+//   Logs messages to the event log
+//
+// Parameters:
+//   szFunction - name of function that failed
+// 
+// Return value:
+//   None
+//
+// Remarks:
+//   The service must have an entry in the Application event log.
+//
+VOID SvcReportEvent(LPTSTR szFunction) 
+{ 
+
 }
